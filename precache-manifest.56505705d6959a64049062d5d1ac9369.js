8179fda05f04dd77547770728768b48ba60c1d1e
rebeccasun <sun.ruoy@husky.neu.edu>
2020-07-06 12:52:17 -0700

    Updates

---
+++ precache-manifest.56505705d6959a64049062d5d1ac9369.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c35512caa50bf03589893dfa115852e4",
+    "url": "/my-web/index.html"
+  },
+  {
+    "revision": "d2a6cfeb29e57140135e",
+    "url": "/my-web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "360c60d906c5833ea3e0",
+    "url": "/my-web/static/js/2.7eccbe67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-web/static/js/2.7eccbe67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2a6cfeb29e57140135e",
+    "url": "/my-web/static/js/main.cf5f70b5.chunk.js"
+  },
+  {
+    "revision": "5b1b7f907bbee126f38d",
+    "url": "/my-web/static/js/runtime-main.4abdfd6f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
